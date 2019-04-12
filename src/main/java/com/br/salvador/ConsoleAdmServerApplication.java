d0a27ba7c9533c16197bbe3f5520da8331fbfa37
TSalvador <thiago.salvadorpower@gmail.com>
2019-04-12 00:27:02 -0300

    Criacao inicial estrutura

---
+++ src/main/java/com/br/salvador/ConsoleAdmServerApplication.java
@@ -0,0 +1,13 @@
+package com.br.salvador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoleAdmServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsoleAdmServerApplication.class, args);
+	}
+
+}
